5c035618eb4ccdbd8c9e28d822d3cad6664b926a
crazycode <crazycode@gmail.com>
2008-09-02 12:25:40 +0800

    还是用C-C o使用快捷键比较方便.

--- vendor/80emacsonrails/emacs-rails/rails.el
+++ vendor/80emacsonrails/emacs-rails/rails.el
@@ -418,7 +418,7 @@ necessary."
             (local-set-key (if rails-use-another-define-key
                                (kbd "TAB") (kbd "<tab>"))
                            'indent-and-complete)
-            (local-set-key (rails-key "a") '(lambda()
+            (local-set-key (rails-key "o") '(lambda()
                                               (interactive)
                                               (mouse-major-mode-menu (rails-core:menu-position))))
             (local-set-key (kbd "C-:") 'ruby-toggle-string<>simbol)
