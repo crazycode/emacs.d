32e43034faaecdd718501a437ec09883afd04651
crazycode <crazycode@gmail.com>
2008-12-27 19:38:24 +0800

    add ruby-electric.el, and remove ruby-block.el.

--- config/70rinari/ruby-support.el
+++ config/70rinari/ruby-support.el
@@ -1,15 +1,5 @@
 (require 'ruby-test)
 
-(require 'ruby-block)
-(ruby-block-mode t)
-;; do overlay
-(setq ruby-block-highlight-toggle 'overlay)
-;; display to minibuffer
-(setq ruby-block-highlight-toggle 'minibuffer)
-;; display to minibuffer and do overlay
-(setq ruby-block-highlight-toggle t)
-
-
 (require 'anything)
 (require 'anything-rcodetools)
 ;; Command to get all RI entries.
@@ -19,4 +9,18 @@
 (require 'anything-dabbrev-expand)
 (global-set-key "\M-/" 'anything-dabbrev-expand)
 (define-key anything-dabbrev-map "\M-/" 'anything-dabbrev-find-all-buffers)
+
+(add-hook 'ruby-mode-hook  
+          (lambda()  
+            (add-hook 'local-write-file-hooks  
+                      '(lambda()  
+                         (save-excursion  
+                           (untabify (point-min) (point-max))  
+                           (delete-trailing-whitespace)  
+                           )))  
+            (set (make-local-variable 'indent-tabs-mode) 'nil)  
+            (set (make-local-variable 'tab-width) 2)  
+            (require 'ruby-electric)  
+            (ruby-electric-mode t)  
+            ))  
 
