2c32f51d6117d749e2617637004e2eda529aa3bf
crazycode <crazycode@gmail.com>
2009-02-04 10:34:20 +0800

    change emacs22's font set.

--- config/01base/font.el
+++ config/01base/font.el
@@ -10,7 +10,16 @@
                        'cjk-misc '("Microsoft YaHei"  . "unicode-bmp"))
      (set-fontset-font (frame-parameter nil 'font)
                        'bopomofo '("Microsoft YaHei"  . "unicode-bmp")) 
-     ))
+     )
+(create-fontset-from-fontset-spec
+"-*-courier-medium-R-normal--14-*-*-*-*-*-fontset-mymono,
+chinese-gb2312:-*-wenquanyi bitmap song-medium-*-normal--14-*-*-*-*-*-iso10646-1,
+chinese-gbk:-*-wenquanyi bitmap song-medium-*-normal--14-*-*-*-*-*-iso10646-1,
+chinese-gb18030:-*-wenquanyi bitmap song-medium-*-normal--14-*-*-*-*-*-iso10646-1"
+)
+(setq default-frame-alist (append '((font . "fontset-mymono")) default-frame-alist))
+(set-default-font "fontset-mymono")
+)
 
 ;; (set-frame-font "DejaVu Sans Mono-10")
 ;; (set-fontset-font (frame-parameter nil 'font)
